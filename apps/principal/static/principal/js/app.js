347c0c061821581a1f5498df3bef00658d517715
Marcos Cisterna <m.cisternab@alumnos.duoc.cl>
2019-10-31 17:28:15 -0300

    Se agregan nuevas fuentes

--- apps/principal/static/principal/js/app.js
+++ apps/principal/static/principal/js/app.js
@@ -1,21 +1,65 @@
 $(document).ready(function(){
-    $("#registro").validate({
+    $("#login").validate({
         errorClass: 'is-invalid',
         rules:{
             username:{
-                required:true
-            },
-            apellido:{
                 required:true
             },
             password: "required",
             password2: {
             required: true,
             equalTo: "#password"
+            }
+           
+        },
+        messages:{
+            username:{
+                required:"Se debe ingresar el nombre de usuario"
             },
-            correo:{
-                required:true,
-                email:true
+            password: {
+                required: 'Falta ingresar la contraseña',
+                minlength: 'Largo insuficiente'
+              },
+              password2: {
+                required: 'Falta reingresar la contraseña',
+                equalTo: 'Las contraseñas ingresadas no coinciden',
+                minlength: 'Largo insuficiente'
+        
+            }
+           
+        }
+    })
+})
+$("#login").submit(function(){
+    if($("#login").valid()){
+        return true;
+    }else{
+        Swal.fire({
+            type: 'error',
+            title: 'Oops...',
+            text: 'Te faltaron ingresar algunos datos!',
+            footer: '<a href>¿Haz olvidado tu contraseña?</a>'
+          })
+    }
+    return false
+})
+
+
+$(document).ready(function(){
+    $("#registro").validate({
+        errorClass: 'is-invalid',
+        rules:{
+            username:{
+                required: true
+            },
+            password: "required",
+            password2: {
+            required: true,
+            equalTo: "#password"
+            },
+            email:{
+                required: true,
+                email: true
             },
             fnacimiento:{
                 required:true
@@ -26,14 +70,10 @@ $(document).ready(function(){
             ncontacto:{
                 required:true
             }
-           
         },
         messages:{
             username:{
-                required:"Se debe ingresar el nombre de usuario"
-            },
-            apellido:{
-                required:"Se debe ingresar el apellido"
+                required:"¡Yo esperaba una dirección de correo!"
             },
             password: {
                 required: 'Falta ingresar contraseña',
@@ -45,15 +85,12 @@ $(document).ready(function(){
                 minlength: 'Largo insuficiente'
         
             },
-            correo:{
+            email:{
                 required:"¡Yo esperaba una dirección de correo!",
                 email: "¡La dirección de correo no es válida!"
             },
             fnacimiento:{
                 required: "Se debe ingresar fecha de nacimiento"
-            },
-            cperros:{
-                required: "Se debe ingresar cantidad de perros"
             },
             Comuna:{
                 required: "Le falta ingresar la comuna"
@@ -72,49 +109,9 @@ $("#registro").submit(function(){
     }else{
         Swal.fire({
             type: 'error',
-            title: 'Oops...',
-            text: 'Te faltaron ingresar algunos datos!',
+            title:'Oops',
+            text: 'Te faltaron ingresar algunos datos',
             footer: '<a href>¿Tienes algun problema?</a>'
-          })
-    }
-    return false
-})
-
-
-$(document).ready(function(){
-    $("#Inicio").validate({
-        errorClass: 'is-invalid',
-        rules:{
-            username:{
-                required: true,
-                email: true
-            },
-            contraseña:{
-                required:true
-            }
-        },
-        messages:{
-            username:{
-                required:"¡Yo esperaba una dirección de correo!",
-                email: "¡La dirección de correo no es válida!"
-            },
-            contraseña:{
-                required:"Debe ingresar su contraseña"
-            }
-           
-
-        }
-    })
-})
-$("#Inicio").submit(function(){
-    if($("#Inicio").valid()){
-        return true;
-    }else{
-        Swal.fire({
-            type: 'error',
-            title:'Los datos son incorrectos',
-            text: 'Por favor, ingresa correctamente los datos ',
-            footer: '<a href>¿Haz olvidado tu contraseña?</a>'
           })
     }
     return false
